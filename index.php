37b478fd1cb8056f58b6cb7858349d8367daa98c
Bill Newman <william.newman@seattlecolleges.edu>
2021-06-03 11:21:43 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
  
 if(isset($_GET['FirstName'])){//show data
-   echo $_GET['FirstName'];
+   echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
 }else{//show form
  echo '
  <form action="">
 First Name: <input type="text" name="FirstName" /><br />
+Last Name: <input type="text" name="LastName" /><br />
 <input type="submit" /> 
  </form>
  ';
