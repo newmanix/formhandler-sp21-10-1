6287dc747a978f91f489113b742c9df0f346a51c
Bill Newman <william.newman@seattlecolleges.edu>
2021-06-03 11:26:55 -0700

    adds var dump

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php
  
 if(isset($_GET['FirstName'])){//show data
-   echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
+   //echo $_GET['FirstName'] . ' ' . $_GET['LastName'];
+
+   echo '<pre>';
+   var_dump($_GET);
+   echo '</pre>';
+
 }else{//show form
  echo '
  <form action="">
